41fd10f83f12b2e681ea612866b46d015006665a
Pantonshire <tom@tomandtally.co.uk>
2020-11-14 16:56:35 +0000

    Search api implemented

--- api/router.go
+++ api/router.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+  "encoding/json"
+  "fmt"
   "github.com/go-chi/chi"
   "github.com/go-chi/chi/middleware"
   "net/http"
@@ -37,16 +39,35 @@ func mwCreatures(path string, f creaturesAPIFunc) http.HandlerFunc {
     nStr := vs.Get("n")
     n, err := strconv.Atoi(nStr)
     if err != nil {
-      panic(err)
+      w.WriteHeader(http.StatusBadRequest)
+      return
     }
     f(w, r, c, n, vs)
   }
 }
 
 func randomAPI(w http.ResponseWriter, r *http.Request, c creatures, n int, vs url.Values) {
-
+  if _, err := fmt.Fprintf(w, "do i look like i'm implemented yet"); err != nil {
+    panic(err)
+  }
 }
 
 func searchAPI(w http.ResponseWriter, r *http.Request, c creatures, n int, vs url.Values) {
-
+  q := vs.Get("q")
+  sStr := vs.Get("s")
+  sPcnt, err := strconv.Atoi(sStr)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+  s := float64(sPcnt) / 100.0
+  if s < 0 {
+    s = 0
+  } else if s > 1 {
+    s = 1
+  }
+  res := c.search(q, n, s)
+  if err := json.NewEncoder(w).Encode(res); err != nil {
+    panic(err)
+  }
 }
