aac23ea76c1cb726fb318f9859e76b5e12623897
Pantonshire <tom@tomandtally.co.uk>
2020-11-14 14:47:23 +0000

    Main file and Makefile

---
+++ api/cmd/main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "context"
+  "github.com/mrlaurence/Tech-Support-Gurus-Oxford-Hack-2020/api"
+  "os"
+  "os/signal"
+  "syscall"
+  "time"
+)
+
+func main() {
+  interrupt := make(chan os.Signal, 1)
+  signal.Notify(interrupt, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+  defer signal.Stop(interrupt)
+
+  err, fatal, shutdown := api.Serve("config.json")
+  if err != nil {
+    panic(err)
+  }
+
+  select {
+  case <-interrupt:
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+    defer cancel()
+    if err := shutdown(ctx); err != nil {
+      panic(err)
+    }
+  case err = <-fatal:
+    panic(err)
+  }
+}
