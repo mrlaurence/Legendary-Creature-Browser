383a1234cf3e5a0ed63475ac93f4c7e34d4f806a
Laurence <laurence@dhonau.uk>
2020-11-14 18:36:34 +0000

    Intergrated with API

--- web/js/creature_browser.js
+++ web/js/creature_browser.js
@@ -2,7 +2,7 @@ $('.carousel').carousel({
   interval: false
 })
 
-var apiLinkBase = "http://127.0.0.1/api";
+var apiLinkBase = "http://127.0.0.1";
 
 function runSearch() {
 
@@ -14,11 +14,11 @@ function runSearch() {
 
   var url = apiLinkBase + "/search?n=" + resultsNo + "&q=" + escape(searchTerms) + "&s=" + sensitivity * 100;
 
-  // xmlHttp.open("GET", url, false); // false for synchronous request
-  // xmlHttp.send(null);
-  // creatureData = xmlHttp.responseText;
+  xmlHttp.open("GET", url, false); // false for synchronous request
+  xmlHttp.send(null);
+  creatureData = xmlHttp.responseText;
 
-  var creatureData = '{"Dragon":{"Description":["Four-legged","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/2/28/Bilibin_Alkonost.JPG","Link":"https://wikipedia.org/blah"},"Ogre":{"Description":["Shrek","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Ammit.svg/800px-Ammit.svg.png","Link":"https://wikipedia.org/blah"}}';
+  //var creatureData = '{"Dragon":{"Description":["Four-legged","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/2/28/Bilibin_Alkonost.JPG","Link":"https://wikipedia.org/blah"},"Ogre":{"Description":["Shrek","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Ammit.svg/800px-Ammit.svg.png","Link":"https://wikipedia.org/blah"}}';
 
   var creaturesObject = JSON.parse(creatureData);
 
@@ -27,24 +27,27 @@ function runSearch() {
   caroInner.innerHTML = "";
 
   var firstItem = true;
-  console.log(creaturesObject);
 
-  Object.keys(creaturesObject).forEach(function(key, index) {
+  if (Object.keys(creaturesObject).length === 0 && creaturesObject.constructor === Object) {
+    caroInner.innerHTML = "<div style='text-align:center'><h2>No results :(</h2></div>";
+  }
+  else {
+    Object.keys(creaturesObject).forEach(function(key, index) {
 
-    var creatureName = key;
-    var creatureTagString = creaturesObject[key].Description.join(', ');
-    var creatureWikiLink = creaturesObject[key].Link;
-    var creatureImageLink = creaturesObject[key].Img;
+        var creatureName = key;
+        var creatureTagString = creaturesObject[key].Description.join(', ');
+        var creatureWikiLink = creaturesObject[key].Link;
+        var creatureImageLink = creaturesObject[key].Img;
 
-    if (firstItem) {
-      firstItem = false;
+        if (firstItem) {
+          firstItem = false;
 
-      caroInner.innerHTML += '<div class="carousel-item active"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
-    } else {
-      caroInner.innerHTML += '<div class="carousel-item"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
-    }
-  });
-}
+          caroInner.innerHTML += '<div class="carousel-item active"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
+        } else {
+          caroInner.innerHTML += '<div class="carousel-item"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
+        }
+      })
+}};
 
 $('#search-form').submit(function() {
   runSearch();
@@ -59,11 +62,11 @@ function getRandom() {
 
   var url = apiLinkBase + "/random?n=" + resultsNo;
 
-  // xmlHttp.open("GET", url, false); // false for synchronous request
-  // xmlHttp.send(null);
-  // creatureData = xmlHttp.responseText;
+  xmlHttp.open("GET", url, false); // false for synchronous request
+  xmlHttp.send(null);
+  creatureData = xmlHttp.responseText;
 
-  var creatureData = '{"Dragon":{"Description":["Four-legged","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/2/28/Bilibin_Alkonost.JPG","Link":"https://wikipedia.org/blah"},"Ogre":{"Description":["Shrek","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Ammit.svg/800px-Ammit.svg.png","Link":"https://wikipedia.org/blah"}}';
+  //var creatureData = '{"Dragon":{"Description":["Four-legged","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/2/28/Bilibin_Alkonost.JPG","Link":"https://wikipedia.org/blah"},"Ogre":{"Description":["Shrek","sexy","fucked a donkey"],"Img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Ammit.svg/800px-Ammit.svg.png","Link":"https://wikipedia.org/blah"}}';
 
   var creaturesObject = JSON.parse(creatureData);
 
@@ -72,21 +75,24 @@ function getRandom() {
   caroInner.innerHTML = "";
 
   var firstItem = true;
-  console.log(creaturesObject);
 
-  Object.keys(creaturesObject).forEach(function(key, index) {
+  if (Object.keys(creaturesObject).length === 0 && creaturesObject.constructor === Object) {
+    caroInner.innerHTML = "<div style='text-align:center'><h2>No results :(</h2></div>";
+  }
+  else {
+    Object.keys(creaturesObject).forEach(function(key, index) {
 
-    var creatureName = key;
-    var creatureTagString = creaturesObject[key].Description.join(', ');
-    var creatureWikiLink = creaturesObject[key].Link;
-    var creatureImageLink = creaturesObject[key].Img;
+        var creatureName = key;
+        var creatureTagString = creaturesObject[key].Description.join(', ');
+        var creatureWikiLink = creaturesObject[key].Link;
+        var creatureImageLink = creaturesObject[key].Img;
 
-    if (firstItem) {
-      firstItem = false;
+        if (firstItem) {
+          firstItem = false;
 
-      caroInner.innerHTML += '<div class="carousel-item active"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
-    } else {
-      caroInner.innerHTML += '<div class="carousel-item"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
-    }
-  });
-}
+          caroInner.innerHTML += '<div class="carousel-item active"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
+        } else {
+          caroInner.innerHTML += '<div class="carousel-item"><div class="creature-div"><h1>' + creatureName + '</h1><div class="creature-col"><p>Tags: ' + creatureTagString + '</p><p>Wikipedia page: <a href="' + creatureWikiLink + '">' + creatureName + '</a></p></div><div class="creature-col"><img src="' + creatureImageLink + '" /></div></div></div>'
+        }
+      })
+}};
