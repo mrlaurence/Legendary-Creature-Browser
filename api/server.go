d24e5856c3af00eb3f9b79831343a742f24bc447
Pantonshire <tom@tomandtally.co.uk>
2020-11-14 14:05:38 +0000

    Serve function

--- api/server.go
+++ api/server.go
@@ -1,7 +1,41 @@
 package api
 
+import (
+  "context"
+  "fmt"
+  "net/http"
+  "time"
+)
+
 type config struct {
   Port uint `json:"port"`
+}
+
+type ShutdownFunc func(context.Context) error
+
+func Serve() (error, <-chan error, ShutdownFunc) {
+  c, err := readConfig("config.json")
+  if err != nil {
+    return err, nil, nil
+  }
+
+  server := http.Server{
+    Addr:        fmt.Sprintf(":%d", c.Port),
+    ReadTimeout: time.Second * 10,
+    Handler:     makeAPIHandler(),
+  }
+
+  fatal := make(chan error, 1)
+
+  go func() {
+    if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+      fatal <- err
+    }
+  }()
+
+  return nil, fatal, func(ctx context.Context) error {
+    return server.Shutdown(ctx)
+  }
 }
 
 func readConfig(path string) (config, error) {
@@ -10,4 +44,8 @@ func readConfig(path string) (config, error) {
     return config{}, err
   }
   return c, nil
+}
+
+func makeAPIHandler() http.Handler {
+  panic("Not implemented")
 }
